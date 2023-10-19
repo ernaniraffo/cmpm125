19c07a478d05c401ec7129a770304b86a6dee9eb
ernaniraffo <ernani.raffo@icloud.com>
2023-10-18 22:28:35 -0700

    Physics World Game

--- Physics-World/Assets/Scripts/Player.cs
+++ Physics-World/Assets/Scripts/Player.cs
@@ -5,35 +5,38 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5;
+	public float speed = 5;
 
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+	private Rigidbody rb;
+	private Ray downwardsRay;
+	private float movementX;
+	private float movementY;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		downwardsRay = new Ray(transform.position, transform.up * -1);
+	}
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //}
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.DrawRay(transform.position, downwardsRay.direction, Color.red);
+	}
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+	void OnMove(InputValue movementValue)
+	{
+		Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+		movementX = movementVector.x;
+		movementY = movementVector.y;
+	}
 
-    // Code for Physics
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
-    }
+	// Code for Physics
+	void FixedUpdate()
+	{
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+		rb.AddForce(movement * speed);
+	}
 }
